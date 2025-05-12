58d397c44827bbef76b764b707c13028741a2c27
Alex94353 <110936746+Alex94353@users.noreply.github.com>
2025-05-12 03:06:43 +0200

    add database connection class
    Trieda na pripojenie k databaze cez PDO

---
+++ classes/Database.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/../config/Settings.php';
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    protected function connect()
+    {
+        $config = Settings::DATABASE; 
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        try {
+            $this->conn = new PDO(
+                'mysql:host=' . $config['HOST'] .
+                ';dbname=' . $config['DBNAME'] .
+                ';port=' . $config['PORT'],
+                $config['USER_NAME'],
+                $config['PASSWORD'],
+                $options
+            );
+        } catch (PDOException $e) {
+            die('Chyba pripojenia: ' . $e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+
+    }
+}
---
+++ config/Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+class Settings
+{
+    public const DATABASE = [
+        'HOST' => 'localhost',
+        'DBNAME' => 'city_portal',
+        'PORT' => 3306,
+        'USER_NAME' => 'root',
+        'PASSWORD' => ''
+    ];
+}
